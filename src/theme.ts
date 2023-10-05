a91059feeb6008507b436a71a1f7146a7a9eff9b
chris-yoo <ysm020423@gmail.com>
2023-10-05 22:40:38 +0900

    settings

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  bgColor: "rgb(19, 32, 67)",
+  cardColor: "rgb(31, 65, 114)",
+  accentColor: "rgb(241, 180, 187)",
+  textColor: "white",
+};
